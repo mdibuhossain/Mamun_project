8f1bbe9e85a980c0de6804d938d47ed8067181f0
Ibrahim H <mohammad.ihakash@gmail.com>
2022-05-14 04:39:40 +0600

    complete

--- main.c
+++ main.c
@@ -266,45 +266,79 @@ void SellMedicine()
     }
 }
 
+void reWriteInfo(Medicine med)
+{
+    Medicine *tmpMed = (Medicine *)calloc(1, sizeof(Medicine));
+    int index = 0;
+    int i = 0;
+    openFile("rb");
+    while (fread(&tmpMed[index], sizeof(Medicine), 1, medicineData) == 1)
+    {
+        tmpMed = (Medicine *)realloc(tmpMed, (index + 2) * sizeof(Medicine));
+        index++;
+    }
+    closeFile();
+
+    openFile("wb");
+    for (i = 0; i < index; i++)
+    {
+        if (strcmp(med.medicneName, tmpMed[i].medicneName) == 0)
+            fwrite(&med, sizeof(Medicine), 1, medicineData);
+        else
+            fwrite(&tmpMed[i], sizeof(Medicine), 1, medicineData);
+    }
+    closeFile();
+    free(tmpMed);
+}
+
 void EnterInfoAboutMedicine()
 {
     int i, flag = 0, c;
-    char name[100], info[100];
+    char name[MAX_SIZEOF_MEDICINE], info[MAX_SIZEOF_INFO];
+    Medicine medData;
     printf("Enter Name of the medicine you want to Review or include Info\n");
     fflush(stdin);
     gets(name);
-    for (i = 0; i < 5; i++)
+
+    openFile("rb");
+    while (fread(&medData, sizeof(Medicine), 1, medicineData) == 1)
     {
-        if (strcmp(m[i].medicneName, name) == 0)
+        if (strcmp(medData.medicneName, name) == 0)
         {
             flag = 1;
-            printf("These are the details of Medicine\n");
-            printf("Name=%s\nPrice=%d\nAvailable Quantity=%d\nCompany=%s\nMfg Date=%s\nExp Date=%s\n", m[i].medicneName, m[i].price, m[i].quantity, m[i].Company, m[i].Mfg_Date, m[i].Exp_Date);
-            if (strcmp(m[i].info, "") != 0)
-            {
-                printf("Review Already Available!\nIf you want to Add a Review Enter 1 else Any other number\n");
-                fflush(stdin);
-                scanf("%d", &c);
-            }
-            else
-            {
-                printf("Give a short review(less than 100 Characters)\n");
-                fflush(stdin);
-                gets(m[i].info);
-            }
-            if (c == 1)
-            {
-                printf("Give a review(less than 100 Characters)\n");
-                fflush(stdin);
-                gets(info);
-                strcat(m[i].info, "; ");
-                strcat(m[i].info, info);
-            }
+            break;
         }
     }
+    closeFile();
+
     if (flag == 0)
     {
         printf("Entered Name of Medicine Not Found\n");
+    }
+    else
+    {
+        printf("These are the details of Medicine\n");
+        dataDisplayInterface(medData);
+        if (strcmp(medData.info, "") != 0)
+        {
+            printf("Review Already Available!\nIf you want to Add a Review Enter 1 else Any other number\n");
+            fflush(stdin);
+            scanf("%d", &c);
+        }
+        else
+        {
+            printf("Give a short review(less than 100 Characters)\n");
+            fflush(stdin);
+            gets(medData.info);
+            reWriteInfo(medData);
+        }
+        if (c == 1)
+        {
+            printf("Give a short review(less than 100 Characters)\n");
+            fflush(stdin);
+            gets(medData.info);
+            reWriteInfo(medData);
+        }
     }
 }
 void KnowInfoAboutMedicine()
